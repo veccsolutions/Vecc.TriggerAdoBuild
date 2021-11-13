e181cd1b248ebf065f22930be9c6b57eeeedc335
Edward Cooke <edward@frakkingsweet.com>
2021-11-12 20:09:04 -0700

    Initial commit

---
+++ Vecc.TriggerAdoBuild/Controllers/TriggerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Vecc.TriggerAdoBuild.Models;
+
+namespace Vecc.TriggerAdoBuild.Controllers
+{
+    [Route("api/trigger")]
+    [ApiController]
+    public class TriggerController : ControllerBase
+    {
+        private readonly ILogger<TriggerController> _logger;
+
+        public TriggerController(ILogger<TriggerController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index([FromQuery] Blob blob)
+        {
+            this._logger.LogInformation("Triggering build on: {blob}", blob.Url);
+            var encodedPat = Convert.ToBase64String(Encoding.UTF8.GetBytes($"something:{blob.Pat}"));
+            var httpClient = new HttpClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, blob.Url);
+            requestMessage.Content = new StringContent("{}", Encoding.UTF8, "application/json");
+            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", encodedPat);
+            try
+            {
+                await httpClient.SendAsync(requestMessage);
+                return this.Ok();
+            }
+            catch (Exception exception)
+            {
+                this._logger.LogError(exception, "Error posting to URL: {url}", blob.Url);
+                return this.StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Vecc.TriggerAdoBuild/Models/Blob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Vecc.TriggerAdoBuild.Models
+{
+    public class Blob
+    {
+        [JsonPropertyName("pat")]
+        public string Pat { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
